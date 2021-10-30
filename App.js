d7cdf95bb32b668f1bfc1913f55220f0ce3c2796
Klevin Bazaiti <klevinbazaiti@Klevins-MacBook-Pro.local>
2021-10-31 00:02:24 +0200

    Add main app component with all the functionality for todos

--- App.js
+++ App.js
@@ -1,13 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
+import Header from './components/Header';
+import TodoItem from './components/TodoItem';
+import AddTodo from './components/AddTodo';
 
 export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  const onHandlePress = (key) => {
+    setTodos((oldTodos) => {
+      return oldTodos.filter(todo => todo.key !== key);
+    })
+  };
+
+  const submitHandler = (todo) => {
+    setTodos((oldTodos) => {
+      return [
+        { name: todo, key: Math.random().toString() },
+        ...oldTodos
+      ]
+    });
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss();
+    }}>
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem item={item} onHandlePress={onHandlePress} />
+              )}
+            />
+          </View>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -15,7 +55,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  content: {
+    padding: 40,
+    flex: 1,
+  },
+  list: {
+    marginTop: 20,
+    flex: 1,
+  }
 });
